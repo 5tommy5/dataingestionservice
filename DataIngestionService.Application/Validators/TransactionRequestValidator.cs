5e27af0713d69d43a23a9447644216bbf8d1d31b
Anton Tomchuk <tomchuk.anton1@gmail.com>
2026-04-14 01:57:05 +0300

    upd: transaction validator

---
+++ DataIngestionService.Application/Validators/TransactionRequestValidator.cs
@@ -0,0 +1,31 @@
+using DataIngestionService.Application.DTOs;
+using FluentValidation;
+using ISO._4217;
+
+namespace DataIngestionService.Application.Validators;
+
+public class TransactionRequestValidator : AbstractValidator<TransactionRequest>
+{
+    public TransactionRequestValidator()
+    {
+        RuleFor(x => x.CustomerId)
+            .NotEmpty();
+
+        RuleFor(x => x.TransactionDate)
+            .NotEmpty()
+            .LessThanOrEqualTo(_ => DateTime.UtcNow.Date);
+
+        RuleFor(x => x.Amount)
+            .GreaterThan(0)
+            .Must(a => decimal.Round(a, 2) == a)
+            .WithMessage("Amount must have at most 2 decimal places.");
+
+        RuleFor(x => x.Currency)
+            .NotEmpty()
+            .Must(c => CurrencyCodesResolver.GetCurrenciesByCode(c).Any())
+            .WithMessage(x => $"Invalid currency code: {x.Currency}");
+
+        RuleFor(x => x.SourceChannel)
+            .NotEmpty();
+    }
+}
