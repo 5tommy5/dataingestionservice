2408e53698ba0c1b7ebe1452d1e8f0fb028026b5
Anton Tomchuk <tomchuk.anton1@gmail.com>
2026-04-14 02:23:31 +0300

    upd: initial migration

---
+++ DataIngestionService.Infrastructure/Migrations/20260413232236_InitialCreate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataIngestionService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "transactions",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    customer_id = table.Column<string>(type: "text", nullable: false),
+                    transaction_date = table.Column<DateTime>(type: "timestamp", nullable: false),
+                    amount = table.Column<decimal>(type: "numeric(18,4)", nullable: false),
+                    currency = table.Column<string>(type: "char(3)", nullable: false),
+                    source_channel = table.Column<string>(type: "text", nullable: false),
+                    idempotency_key = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_transactions", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transactions_customer_id",
+                table: "transactions",
+                column: "customer_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transactions_idempotency_key",
+                table: "transactions",
+                column: "idempotency_key",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "transactions");
+        }
+    }
+}
